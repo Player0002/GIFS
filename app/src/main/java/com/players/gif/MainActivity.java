769f0fc23eefc74233218aea65791738e43f5308
KyungHoon0126 <48943501+KyungHoon0126@users.noreply.github.com>
2019-10-15 11:49:02 +0900

    test

--- app/src/main/java/com/players/gif/MainActivity.java
+++ app/src/main/java/com/players/gif/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//ASF
+        super.onCreate(savedInstanceState);//ASFdsfdsf
         setContentView(R.layout.activity_main);
     }
 }
