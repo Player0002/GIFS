1332a84f544b36223cc889a8f259982062897ca3
김영재 <dudwo0914@gmail.com>
2019-10-21 13:17:01 +0900

    Facebook api DATA GET TEST

--- app/src/main/java/com/players/gif/TestActivity.java
+++ app/src/main/java/com/players/gif/TestActivity.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
 
 public class TestActivity extends AppCompatActivity {
 
+    /*
+
+        DATA GET
+        https://graph.facebook.com/v4.0/me?fields=id,name,gender,email,events&access_token={Access token}
+
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         LoginManager.getInstance().logOut();
+        print(AccessToken.getCurrentAccessToken().getToken());
         new Thread(()->{
            try{
                 Thread.sleep(1000);
@@ -21,6 +30,9 @@ public class TestActivity extends AppCompatActivity {
            }catch(Exception e){
                e.printStackTrace();
            }
-        });
+        }).start();
+    }
+    private void print(String s) {
+        Log.println(Log.DEBUG, "DATA - > ", s);
     }
 }
