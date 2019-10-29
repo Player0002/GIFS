6cc4df02885911b1af39b2c8987110bf9779dfb4
김영재 <dudwo0914@gmail.com>
2019-10-29 17:16:16 +0900

    Add google sign in

--- app/src/main/java/com/players/gif/LoginActivity.java
+++ app/src/main/java/com/players/gif/LoginActivity.java
@@ -4,19 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.squareup.picasso.Picasso;
 
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String GOOGLE_ACCOUNT = "google_account";
+    private TextView textView;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textView = findViewById(R.id.showData);
+        imageView = findViewById(R.id.profileImage);
+        findViewById(R.id.login).setOnClickListener((v)->{
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra(MainActivity.wantSignOut, true);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
+        setShowData();
+    }
+    private void setShowData(){
+        GoogleSignInAccount account = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+        //Picasso.get().load(account.getPhotoUrl()).centerInside().into(imageView);
+        Log.d("DATA : ", account.getPhotoUrl().toString());
+        textView.setText(account.getDisplayName() + " / " + account.getEmail());
     }
 }
--- app/src/main/java/com/players/gif/MainActivity.java
+++ app/src/main/java/com/players/gif/MainActivity.java
@@ -1,7 +1,12 @@
 package com.players.gif;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,6 +15,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.players.gif.ui.DashboardFragment;
 import com.players.gif.ui.TestFragment;
@@ -20,10 +32,34 @@ public class MainActivity extends AppCompatActivity {
     /*private FragmentManager fragmentManager = getSupportFragmentManager();
     private TestFragment testFragment = new TestFragment();
     private DashboardFragment dashboardFragment = new DashboardFragment();*/
+    private SignInButton googleSignInButton;
+    public static final String wantSignOut = "want_sign_out";
+    private GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//ASFdsfdsf
-        setContentView(R.layout.activity_main);/*BottomNavigationView navView = findViewById(R.id.nav_view);
+        setContentView(R.layout.activity_main);
+
+        googleSignInButton = findViewById(R.id.sign_in_button);
+
+        googleSignInButton.setStyle(SignInButton.SIZE_WIDE, SignInButton.COLOR_AUTO);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestId()
+                .requestEmail().requestProfile()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleSignInButton.setOnClickListener((v)->{
+            Intent signInIntent = googleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, 101);
+        });
+        if(getIntent().getBooleanExtra(wantSignOut, false)){
+            googleSignInClient.signOut().addOnSuccessListener((a)->{
+                Log.w("DATA : ", "SUCCESS");
+            });
+        }
+        /*BottomNavigationView navView = findViewById(R.id.nav_view);
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.nav_host_fragment, testFragment).commitAllowingStateLoss();
@@ -50,5 +86,40 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);*/
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println(requestCode + " / " + resultCode);
+        if(resultCode == Activity.RESULT_OK)
+            switch(requestCode){
+                case 101:
+                    try{
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        onLoggedIn(account);
+                    }catch(ApiException e){
+                        Log.w("ERROR", "SIGN IN RESULT FAILD CODE = " + e.getStatusCode());
+                    }
+                    break;
+            }
+    }
+    private void onLoggedIn(GoogleSignInAccount account){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(LoginActivity.GOOGLE_ACCOUNT, account);
+        startActivity(intent);
+        finish();
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        GoogleSignInAccount alreadyLogin = GoogleSignIn.getLastSignedInAccount(this);
+        if(alreadyLogin != null){
+            Toast.makeText(this, "이미 로그인 하였습니다.", Toast.LENGTH_LONG).show();
+            onLoggedIn(alreadyLogin);
+        }else{
+            Log.d("DATA : ", "NOT LOGIN");
+        }
     }
 }
