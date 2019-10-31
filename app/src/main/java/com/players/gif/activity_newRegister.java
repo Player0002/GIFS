09c6ea38ca97df8fc5ef7a7f1338ac80589df4ba
KyungHoon0126 <48943501+KyungHoon0126@users.noreply.github.com>
2019-10-31 21:09:33 +0900

    activity_profile empty 새로 생성, activity_newRegister 생성

---
+++ app/src/main/java/com/players/gif/activity_newRegister.java
@@ -0,0 +1,14 @@
+package com.players.gif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_newRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_register);
+    }
+}
