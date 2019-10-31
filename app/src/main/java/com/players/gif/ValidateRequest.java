e3c61c0b548d6a74ef232c3cd698bcfc94b5002b
KyungHoon0126 <48943501+KyungHoon0126@users.noreply.github.com>
2019-10-31 22:57:43 +0900

    Class 추가

--- app/src/main/java/com/players/gif/ValidateRequest.java
+++ app/src/main/java/com/players/gif/ValidateRequest.java
@@ -7,13 +7,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-// 현재 ID 체크 담당 기능 - 경훈 -
 public class ValidateRequest extends StringRequest {
 
     final static private String URL = "http://"; // 서버주소 추가할 것.
     private Map<String, String> parameters;
 
-    public VailidateRequest(String userID, String userPassword, String userGender, String userMajor, String userEmail, Response.Listener<String> listener)
+    public ValidateRequest(String userID, String userPassword, String userGender, String userMajor, String userEmail, Response.Listener<String> listener)
     {
         super(Method.POST, URL, listener, null);
         parameters = new HashMap<>();
