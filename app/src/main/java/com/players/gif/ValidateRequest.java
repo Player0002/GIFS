72c515193de9bb06a5aa396d7f76c9690b555346
KyungHoon0126 <48943501+KyungHoon0126@users.noreply.github.com>
2019-10-31 23:00:35 +0900

    Class 오류 수정

--- app/src/main/java/com/players/gif/ValidateRequest.java
+++ app/src/main/java/com/players/gif/ValidateRequest.java
@@ -6,21 +6,17 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-
+// 회원 ID 체크 담당 부분 -  경훈 -
 public class ValidateRequest extends StringRequest {
 
     final static private String URL = "http://"; // 서버주소 추가할 것.
     private Map<String, String> parameters;
 
-    public ValidateRequest(String userID, String userPassword, String userGender, String userMajor, String userEmail, Response.Listener<String> listener)
+    public ValidateRequest(String userID, Response.Listener<String> listener)
     {
         super(Method.POST, URL, listener, null);
         parameters = new HashMap<>();
         parameters.put("userID", userID);
-        parameters.put("userPassword", userPassword);
-        parameters.put("userGender", userGender);
-        parameters.put("userMajor", userMajor);
-        parameters.put("userEmail", userEmail);
     }
 
     @Override
